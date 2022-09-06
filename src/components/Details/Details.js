7dda3d17ffd82bbf98898dc7a3e2e7ba0de6e781
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 09:47:44 +0600

    done single user update

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Details = () => {
     const { id } = useParams();
-
+    const [user, setUser] = useState({})
+    useEffect(() => {
+        const url = `http://localhost:5000/user/${id}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setUser(data))
+    }, [id])
 
     return (
         <div>
-            <h2>{id}</h2>
-
+            <hp>{id}</hp>
+            <h3>{user.name}</h3>
         </div>
     );
 };
