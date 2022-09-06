3a0eafa000003afad6edcad412f21a40e11a9552
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 07:49:47 +0600

    router set up

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 
-import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import User from './components/User/User';
 
 function App() {
-  const [users, setUsers] = useState([])
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data))
-  }, [users])
-
-  const handleForm = (event) => {
-    event.preventDefault()
-    const name = event.target.name.value;
-    const job = event.target.job.value;
-    const user = { name, job }
-
-    fetch('http://localhost:5000/user', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(res => res.json())
-      .then(data => setUsers([...users, data]))
-  }
-
 
   return (
-    <div className='test'>
-      <form onSubmit={handleForm}>
-        <input type="text" name='name' placeholder='name' />
-        <br />
-        <input type="text" name="job" placeholder='job' />
-        <br />
-        <input type="submit" value="Add User" />
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/user/add' element={<User></User>}></Route>
 
-      </form>
-      <ul>
-        {
-          users.map((user, index) => <li key={index}>{user.name}</li>)
-        }
-      </ul>
+      </Routes>
 
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [users])
+
+    const handleForm = (event) => {
+        event.preventDefault()
+        const name = event.target.name.value;
+        const job = event.target.job.value;
+        const user = { name, job }
+
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => setUsers([...users, data]))
+    }
+
+    return (
+        <div className='test'>
+            <form onSubmit={handleForm}>
+                <input type="text" name='name' placeholder='name' />
+                <br />
+                <input type="text" name="job" placeholder='job' />
+                <br />
+                <input type="submit" value="Add User" />
+
+            </form>
+            <ul>
+                {
+                    users.map((user, index) => <li key={index}>{user.name}</li>)
+                }
+            </ul>
+
+        </div>
+    );
+};
+
+export default Home;
