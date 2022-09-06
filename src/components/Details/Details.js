3aa9b0969505100916f0b65803e3e7b649021c30
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-06 10:12:29 +0600

    update user done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -9,12 +9,41 @@ const Details = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setUser(data))
-    }, [id])
+    }, [id, user])
+
+    const handleUpdateForm = (event) => {
+        event.preventDefault()
+        const updateName = event.target.name.value;
+        const updateJob = event.target.job.value;
+        const user = { name: updateName, job: updateJob }
+        const url = `http://localhost:5000/user/${id}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                alert('user update successfully')
+                event.target.reset()
+            })
+    }
 
     return (
         <div>
-            <hp>{id}</hp>
+            <h3>{id}</h3>
             <h3>{user.name}</h3>
+            <h3>{user.job}</h3>
+            <form onSubmit={handleUpdateForm}>
+                <input type="text" name='name' placeholder='name' />
+                <br />
+                <input type="text" name="job" placeholder='job' />
+                <br />
+                <input type="submit" value="Add User" />
+
+            </form>
         </div>
     );
 };
